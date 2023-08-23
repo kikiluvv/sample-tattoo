0c6ac0c8753383bb823c413577326ef2ca1a77ce
kikiluvv <jrwenis@gmail.com>
2023-08-23 08:04:47 -0700

    gallery test 1.2.1

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ app.post('/jotform-webhook', (req, res) => {
     // Extract flashId from the form submission data
     const flashIdFromJotForm = req.body.flashId;
 
+    console.log('Webhook Payload:', req.body);
     try {
         const jsonFilePath = path.join(__dirname, 'data', 'gallery.json');
         const jsonContent = fs.readFileSync(jsonFilePath, 'utf8');
@@ -75,12 +76,16 @@ app.post('/jotform-webhook', (req, res) => {
         // Find and update the flash in your JSON array
         const flashToUpdate = flashArray.find(flash => flash.id === flashIdFromJotForm);
         if (flashToUpdate) {
+            console.log('Updating flash:', flashToUpdate);
             flashToUpdate.claimed = true;
+            console.log('Updated flash:', flashToUpdate);
             // Update your JSON array or database with the change
         }
         fs.writeFileSync(jsonFilePath, JSON.stringify(jsonArray, null, 2));
+        console.log('JSON written to file:', jsonFilePath);
 
         res.sendStatus(200); // Send a success response
+        console.log('Sent response:', 200);
     } catch (error) {
         console.error('Error retrieving form submission data', error);
         res.status(500).json({ error: 'Internal server error' });
