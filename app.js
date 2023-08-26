3ec1f5ac8f462205dc89f7c76ef5b0fc73370abc
kikiluvv <jrwenis@gmail.com>
2023-08-26 11:37:34 -0700

    prod gallery test 1.2.4

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
 
 
 
